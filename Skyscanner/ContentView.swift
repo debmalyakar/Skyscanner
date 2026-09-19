1b259b1f909b0dcee91ff03ee20390b4cf72c95d
debmalyakar <debmalyakar@gmail.com>
2026-09-19 21:11:20 +0530

    Initial Commit

---
+++ Skyscanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skyscanner
+//
+//  Created by debmalyakar on 19/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skyscanner/SkyscannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyscannerApp.swift
+//  Skyscanner
+//
+//  Created by debmalyakar on 19/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SkyscannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
